d59f16aaea56772aa7b2071bb641bd4b9a175a56
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-01 09:31:14 +0700

    Update peminjaman.js

--- routes/peminjaman.js
+++ routes/peminjaman.js
@@ -23,14 +23,14 @@ router.post('/tambah', verifyToken, verifyAdmin, async (req, res) => {
         return res.status(400).json({ message: 'Stok barang habis' });
       }
       
-      const tanggalUTC = moment().utc().toISOString();
+      const tanggalPinjamWIB = moment().tz('Asia/Jakarta').format();
 
   
       const pinjam = await pool.query(
         `INSERT INTO peminjaman 
           (id_barang, peminjam, status, tanggal_pinjam) 
          VALUES ($1, $2, $3, $4) RETURNING *`,
-        [id_barang, peminjam, 'dipinjam', tanggalUTC]
+        [id_barang, peminjam, 'dipinjam', tanggalPinjamWIB]
       );
       
       
