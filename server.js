ce306b646093ce513bfeb346a11ea84e1a43eef5
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-01 10:29:10 +0700

    oke

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server berjalan di http://0.0.0.0:${PORT}`);
