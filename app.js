9c618d01b715291df504af34b74ceb94806c6897
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-01 10:11:23 +0700

    oke

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+// CORS setup dengan opsi spesifik
+const corsOptions = {
+  origin: 'https://frontend-peminjaman-barang.vercel.app',  // Ganti dengan domain frontend Anda
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Metode yang diperbolehkan
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Header yang diizinkan
+};
+
+// Gunakan CORS untuk semua rute dengan opsi yang ditentukan
+app.use(cors(corsOptions));
+
+// Middleware lainnya
 app.use(express.json());
 
 // Routes
@@ -20,5 +31,3 @@ const dashboardRoutes = require('./routes/dashboard');
 app.use('/api/dashboard', dashboardRoutes);
 
 module.exports = app;
-
-
